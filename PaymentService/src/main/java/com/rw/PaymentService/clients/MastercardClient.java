c2b23381ef5885dcf4e2012aa56897c36f86a42e
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-10-24 14:27:47 +0200

    ft: add Feign client interface

--- PaymentService/src/main/java/com/rw/PaymentService/clients/MastercardClient.java
+++ PaymentService/src/main/java/com/rw/PaymentService/clients/MastercardClient.java
@@ -1,8 +1,12 @@
 package com.rw.PaymentService.clients;
 
 import com.rw.PaymentService.config.FeignConfig;
+import com.rw.PaymentService.dtos.PaymentRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "mastercardClient", url = "${mastercard.api.url}", configuration = FeignConfig.class)
 public interface MastercardClient {
@@ -10,5 +14,14 @@ public interface MastercardClient {
     String authenticate();
 
     @PostMapping("/payments/initiate")
-    String initiatePayment();
+    String initiatePayment(@RequestBody PaymentRequest paymentRequest);
+
+    @GetMapping("/payments/status/{transactionId}")
+    String checkTransactionStatus(@PathVariable(name = "transactionId") String transactionId);
+
+    @PostMapping("/payments/refund")
+    String refundPayment(@RequestBody String transactionId);
+
+    @GetMapping("/accounts/{accountId}")
+    String getAccountDetails(@PathVariable(name = "accountId") String accountId);
 }
